87c94c6162a8cc4f64d12fecbb7b8b54632be390
DanielVNZ <123446671+DanielVNZ@users.noreply.github.com>
2024-04-18 18:26:49 +1200

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,160 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.Prefabs;
+using Game.SceneFlow;
+using Game.Simulation;
+using UnityEngine.PlayerLoop;
+using Unity.Entities;
+using Colossal.IO.AssetDatabase;
+using Game.Debug;
+using Game.Settings;
+using UnityEngine;
+using Game.UI;
+using System.Threading.Tasks;
+using Colossal.Serialization.Entities;
+using UnityEngine.Scripting;
+
+
+
+namespace NoPollution
+{
+    public class Mod : IMod
+    {
+        public Setting m_Setting;
+
+        public BruceyPollutionSystem _bruceyPollutionSystem;
+
+        public static ILog log = LogManager.GetLogger($"{nameof(NoPollution)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            if (_bruceyPollutionSystem == null)
+            {
+                _bruceyPollutionSystem = new BruceyPollutionSystem(this);
+                log.Info("Pollution System Found");
+            }
+            World.DefaultGameObjectInjectionWorld.AddSystemManaged(_bruceyPollutionSystem);
+
+            m_Setting = new Setting(this, _bruceyPollutionSystem);
+            m_Setting.RegisterInOptionsUI();
+
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+            AssetDatabase.global.LoadSettings(nameof(NoPollution), m_Setting, new Setting(this, _bruceyPollutionSystem));
+
+            m_Setting.Apply();
+
+            updateSystem.UpdateAt<BruceyPollutionSystem>(SystemUpdatePhase.MainLoop);
+            updateSystem.UpdateAt<BruceyPollutionSystem>(SystemUpdatePhase.PrefabUpdate);
+            updateSystem.UpdateAt<BruceyPollutionSystem>(SystemUpdatePhase.GameSimulation);
+            updateSystem.UpdateAt<BruceyPollutionSystem>(SystemUpdatePhase.ApplyTool);
+
+            
+        }
+
+
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+            if (m_Setting != null)
+            {
+                m_Setting.UnregisterInOptionsUI();
+                m_Setting = null;
+            }
+        }
+
+    }
+
+    public partial class BruceyPollutionSystem : GameSystemBase
+    {
+        public Mod _mod;
+        public NoisePollutionSystem _noisePollutionSystem;
+        public NetPollutionSystem _netPollutionSystem;
+        public BuildingPollutionAddSystem _buildingPollutionAddSystem;
+        public GroundWaterPollutionSystem _groundWaterPollutionSystem;
+        public WaterPipePollutionSystem _waterPipePollutionSystem;
+        public AirPollutionSystem _airPollutionSystem;
+
+        public BruceyPollutionSystem(Mod mod)
+        {
+            _mod = mod;
+            
+
+        }
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+
+        protected override void OnGameLoadingComplete(Purpose purpose, GameMode mode)
+        {
+
+            base.OnGameLoadingComplete(purpose, mode);
+
+            if (!mode.IsGameOrEditor())
+
+                return;
+
+            if (mode.IsGameOrEditor())
+            {
+
+                _noisePollutionSystem = World.GetExistingSystemManaged<NoisePollutionSystem>();
+                _netPollutionSystem = World.GetExistingSystemManaged<NetPollutionSystem>();
+                _buildingPollutionAddSystem = World.GetExistingSystemManaged<BuildingPollutionAddSystem>();
+                _groundWaterPollutionSystem = World.GetExistingSystemManaged<GroundWaterPollutionSystem>();
+                _waterPipePollutionSystem = World.GetExistingSystemManaged<WaterPipePollutionSystem>();
+                _airPollutionSystem = World.GetExistingSystemManaged<AirPollutionSystem>();
+
+            }
+
+        }
+
+        public void TogglePollution(bool Toggle)
+        {
+            if (_noisePollutionSystem != null && _netPollutionSystem != null && _buildingPollutionAddSystem !=null && _groundWaterPollutionSystem != null && _waterPipePollutionSystem != null && _airPollutionSystem != null)
+            {
+                _noisePollutionSystem.Enabled = Toggle;
+                _netPollutionSystem.Enabled = Toggle;
+                _buildingPollutionAddSystem.Enabled = Toggle;
+                _groundWaterPollutionSystem.Enabled = Toggle;
+                _waterPipePollutionSystem.Enabled = Toggle;
+                _airPollutionSystem.Enabled = Toggle;
+                Mod.log.Info("noisePollutionSystem = " + _noisePollutionSystem.Enabled);
+                Mod.log.Info("netPollutionSystem = " + _netPollutionSystem.Enabled);
+
+            }
+            else
+            {
+                Mod.log.Info("Something is null");
+            }
+
+
+        }
+
+        protected override void OnUpdate()
+        {
+
+        }
+
+        public void OnGameExit()
+        {
+
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+
+    }
+}
---
+++ Setting.cs
@@ -0,0 +1,106 @@
+﻿using Colossal;
+using Colossal.IO.AssetDatabase;
+using Game.Modding;
+using Game.Settings;
+using Game.UI;
+using Game.UI.Widgets;
+using System.Collections.Generic;
+
+namespace NoPollution
+{
+    [FileLocation(nameof(NoPollution))]
+    [SettingsUIGroupOrder(kToggleGroup, kButtonGroup)]
+    [SettingsUIShowGroupName(kToggleGroup, kButtonGroup)]
+    public class Setting : ModSetting
+    {
+
+        private readonly Mod _mod;
+        private BruceyPollutionSystem _system;
+       // private BruceyPollutionSystem _pollutionSystem;
+        public bool Toggle1;
+
+
+        public const string kSection = "Main";
+
+        public const string kButtonGroup = "Button";
+        public const string kToggleGroup = "Toggle";
+
+        public Setting(IMod mod, BruceyPollutionSystem bruceySystem) : base(mod)
+        {
+            _mod = (Mod)mod;
+            _system = bruceySystem;
+
+            Toggle1 = Toggle;
+        }
+
+
+
+        [SettingsUISection(kSection, kToggleGroup)]
+        public bool Toggle { get; set; }
+
+        public override void Apply()
+        {
+            base.Apply();
+
+            if (Toggle == true)
+            {
+                Toggle1 = true;
+                Mod.log.Info("Pollution = " + Toggle1);
+            }
+            else
+            {
+                Toggle1 = false;
+                Mod.log.Info("Pollution = " + Toggle1);
+            }
+
+            if (_system != null)
+            {
+                _system.TogglePollution(Toggle1);
+                Mod.log.Info("Pollution Updated from Apply Method. Pollution = " + Toggle1);
+            }
+            else
+            {
+                Mod.log.Info("Pollution or Mod System is null");
+            }
+        }
+
+
+
+
+        public override void SetDefaults()
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+
+    public class LocaleEN : IDictionarySource
+    {
+        private readonly Setting m_Setting;
+        public LocaleEN(Setting setting)
+        {
+            m_Setting = setting;
+        }
+        public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors, Dictionary<string, int> indexCounts)
+        {
+            return new Dictionary<string, string>
+            {
+                { m_Setting.GetSettingsLocaleID(), "No Pollution" },
+                { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
+
+                { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup), "Buttons" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "Toggle" },
+
+
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Toggle)), "Toggle Pollution" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Toggle)), $"Toggles Pollution On or Off" },
+
+            };
+        }
+
+        public void Unload()
+        {
+
+        }
+    }
+}
